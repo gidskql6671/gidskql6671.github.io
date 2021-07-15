773d824995955c87ea3a951b8dd621e60b49cbb7
gidskql6671 <gidskql6671@gmail.com>
2021-07-15 21:17:22 +0900

    드래그 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import { Container } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
-import { Route, Link, Switch, Redirect, withRouter } from 'react-router-dom';
+import './css/App.css';
 
+import Frame from './Components/Frame';
+import { useEffect } from 'react';
 
-
-
-
-const App = ( { location } ) => {
-	let [toggleProfile, setToggleProfile] = useState(true); 
+const App = ( ) => {
+	let x, left, down = false;
+	
+	const listenerMousedown = e => {
+		e.preventDefault();
+		down = true;
+		x = e.pageX;
+		left = document.getElementById("screen").scrollLeft;
+	}
+	
+	
+	const listenerMouseup = e => {
+		down = false;
+		let newX = e.pageX;
+		
+		console.log(window.innerWidth, x, newX);
+		
+		if (Math.abs(newX - x) > window.innerWidth / 4){
+			const screen = document.getElementById("screen");
+			if (newX - x > 0){
+				screen.scrollTo({
+					left: screen.scrollLeft - window.innerWidth, 
+					top: 0,
+					behavior: 'smooth'
+				});
+			}
+			else{
+				screen.scrollTo({
+					left: screen.scrollLeft + window.innerWidth, 
+					top: 0,
+					behavior: 'smooth'
+				});
+			}
+		}
+	}
 	
 	useEffect(() => {
+		window.addEventListener("mousedown", listenerMousedown);
+		window.addEventListener("mouseup", listenerMouseup);
 		
-	}, [location]);
-	
+		return () => {
+			window.removeEventListener("mousedown");
+			window.removeEventListener("mouseup");
+		}
+	})
 	
 	return (
-		<div className="App">
-			<Container fluid className="p-0">
-			</Container>
+		<div className="App" id="screen">
+			<Frame>
+				<h1> 안녕 </h1>
+				<p> asd </p>
+			</Frame>
+			<Frame >
+				<h1> 안녕2 </h1>
+				<p> asd </p>
+			</Frame>
+			
+			<Frame>
+				<h1> 안녕3 </h1>
+				<p> asd </p>
+			</Frame>
 		</div>
 	);
 }
 
-export default withRouter(App);
+export default App;
